36521b4ff59086fb234c74ca0c097f02fcefbd8b
Felipe Robles <felipelmrobles03@gmail.com>
2022-02-07 14:15:50 -0300

    introduction-almost-done

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const instagramTab = () => {
+    const felipeImage = document.querySelector('.felipe-image')
+
+    felipeImage.addEventListener('click', () => {
+        window.open('https://www.instagram.com/feliith/', '_blank')
+    })
+}
+instagramTab()
